4fe5e0ae30743bef71f57ce2c4563dbaf922e442
th3outcast <amatajosh@gmail.com>
2022-09-12 17:39:28 +0100

    add bits2ints

--- src/ecvrf/primitives.rs
+++ src/ecvrf/primitives.rs
@@ -6,4 +6,45 @@ use openssl::{
     error::ErrorStack,
 };
 
-/// Converts an octet string to a non-negative integer a
+/// Converts a sequence of blen bits to a non-negative integer that is less than 2^qlen as specified in
+/// [Section 2.3.2 of \[RFC6979\]](https://www.rfc-editor.org/rfc/rfc6979#section-2.3.2)
+///
+/// # Arguments
+///
+/// * `num`: an octet slice representing the number to be converted
+/// * `qlen`: the length of the output `BigNum`
+///
+/// # returns
+///
+pub fn bits2ints(
+    num: &[u8],
+    qlen: usize,
+) -> Result<BigNum, ErrorStack> {
+    let vlen = num.len() * 8;
+    let result = BigNum::from_slice(num)?;
+
+    if vlen > qlen {
+        let mut truncated = BigNum::new()?;
+        truncated.rshift(&result, (vlen - qlen) as i32)?;
+        
+        Ok(truncated)
+    } else {
+        Ok(result)
+    }
+}
+
+/// Converts a sequence of blen bits to an output of rlen bits as specified in
+/// [Section 2.3.4 of \[RFC6979\]](https://www.rfc-editor.org/rfc/rfc6979#section-2.3.4)
+///
+/// # Arguments
+///
+/// * ``:
+/// * ``:
+///
+/// # returns
+///
+pub fn bits2octets(
+
+) -> Result<> {
+
+}
